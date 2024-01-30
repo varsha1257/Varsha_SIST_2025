8d8038b131de0fb14cddd57de098b621adbc3918
Varsha M <143739640+varsha1257@users.noreply.github.com>
2024-01-30 21:14:25 +0530

    Create ques1.py

---
+++ 29-01-24/ques1.py
@@ -0,0 +1,44 @@
+"""
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+"""
+
+class Solution(object):
+    def isPalindrome(self, x):
+        a=str(x)
+        return a == a[::-1]
+c=Solution()
+
+
+x1=121
+print(c.isPalindrome(x1))
+
+x2=-121
+print(c.isPalindrome(x2))
+
+x3=10
+print(c.isPalindrome(x3))
